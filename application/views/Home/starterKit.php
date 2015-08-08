0070ebae09f121105c6654e64cb3658d8a509445
Louiz Echiverri <louizechiverri@gmail.com>
2015-08-09 02:02:10 +0800

    Initial Commit

---
+++ application/views/Home/starterKit.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>IdeaLab</title>
+        
+        <!-- Import materialize.css -->
+        <link type="text/css" rel="stylesheet" href="assets/css/materialize/css/materialize.css" media="screen, projection"/>
+        
+        <!-- Let browser know website is optimized for mobile -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        
+        
+        <style>
+            .outer {
+                height: 100vh;
+                text-align: center;
+            }
+            
+            .outer:before{
+                content:"";
+                display:inline-block;
+                height:100%;
+                vertical-align:middle;
+            }
+            
+            .inner {
+                width: 66%;
+                display:inline-block;
+                vertical-align:middle;
+            }
+        
+        </style>
+        
+    </head>
+    <body style="overflow: auto">
+        <div class="container">            
+            <div class="row">
+                <h3></h3>
+                <p class="flow-text"></p>
+            </div>
+            <div class="row">
+                <h3>Why <span style="color:#92d050">Idea</span><span style="color:#ffc000">Lab</span></h3>
+                <p class="flow-text">IdeaLab was borne out from a collective narrative of different stories that fell into place: long night talks of reform by quiet members of a student political party, the underground movement of independent students who are fed up of being gagged by the present system in USC, and just now, the cry for a Colorless SSC. People from different walks of life converging together towards a single advocacy—change. </p>
+                <p class="flow-text">It is now apparent that people are hungry for change. Marc Ong’s election was not by accident: for the first time in more than ten years since SPP was established, Carolinians have elected a majority president. And for more than ten years, voter turnout has surpassed the 40 per cent threshold. However, the challenge that we face today is how to move change from being a noun to a verb. To paraphrase Joel Barker, the vision must turn into action in order to change the world; otherwise, a vision without action is just a dream. </p>
+                <p class="flow-text">The vision in IdeaLab is simple: to provide a hub for Carolinians to incubate real and passion-filled advocacies so that we can contribute in producing servant leaders who put service above politics, service above positions, and service above recognitions. We want you to feel comfortable with the idea that you can make a difference without having to earn a position. All you need is an idea and a group of people to help you enable that idea into reality. </p>
+            </div>
+            <div class="row">
+                <h3>How does <span style="color:#92d050">Idea</span><span style="color:#ffc000">Lab</span> work?</h3>
+                <p class="flow-text">IdeaLab serves an incubator for student-led initiatives and advocacies. Implementing a project or starting an advocacy campaign requires the right idea with the right attitude. </p>
+                <h4>The Ideation Program</h4>
+                <p class="flow-text">To get to the right idea, our fellows have to seriously reflect whether they are passionate about their ideas and whether it is something the Carolinian community can embrace. Fellows who are admitted into IdeaLab will face a semester-long Ideation program where they are exposed to different types of ideas, initiatives and advocacies. During weekends, we’ll invite speakers around Cebu and across the Philippines to share their passion stories about their advocacies, organize small TED talk sessions where our fellows listen, reflect and discuss what they’ve heard, conduct an inventory on the situation and needs of the Carolinian community, and provide personal mentors who can guide you through the whole process of incubating your ideas.</p>
+                <h4>The Servant Leadership Formation Program</h4>
+                <p class="flow-text">Running side by side is the Servant Leadership Formation Program. We would to galvanize the identity of a Carolinian Warrior—as advocacy-driven servant leaders. As quiet revolutionaries for change, servant leaders are aware of their capabilities and potentials, who have a long-term vision of what they advocacies to grow and become, who are stewards committed to serve their constituents of their chosen advocacies first, and who yearn to build a diverse Carolinian community. During the weekends, fellows will pass through a series of formation modules, team building exercises, small group workshops, and fellowship activities in order to form the right attitude and perspective in pursuing their chosen advocacies.  
+    </p>
+            </div>
+            <div class="row">
+                <h3></h3>
+                <p class="flow-text"></p>
+            </div>
+            <div class="row">
+                <h3></h3>
+                <p class="flow-text"></p>
+            </div>
+            <div class="row">
+                <h3></h3>
+                <p class="flow-text"></p>
+            </div>
+        </div>
+
+        <!-- Import jquery and materialize.js -->
+        <script type="text/javascript" src="assets/js/jquery-2.1.4.js"></script>
+        <script type="text/javascript" src="assets/js/materialize/js/materialize.js"></script>
+        <script type="text/javascript" src="assets/js/custom/home.js"></script>
+    </body>
+</html>
+
---
+++ assets/js/custom/home.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    $("#coming-soon-head").fadeIn(1000,function(){
+        $("#coming-soon-body").delay(500).fadeIn(1000);
+        $(".page-footer").delay(500).fadeIn(1000);
+    });
+    
+    $("#btn_starterKit").on("click", function(){
+        $(".page-footer").fadeOut();
+        $("#coming-soon-body").fadeOut(function(){
+            $("#coming-soon-starterKit").fadeIn();
+            $(".page-footer").css("margin-top","150px");
+            $(".page-footer").toggle();
+            
+            $('.scrollspy').scrollSpy();
+            $('#navigation_1').pushpin({ top: $('#navigation_1').offset().top });
+        });
+    });
+    
+    $(".nav-btn").on("click", function(){
+        var id = $(this).attr("id");
+        var kitX = null;
+        var navX = null;
+        
+        var kitY = null;
+        var navY = null;
+        
+        if(id == "btn_nav1_next"){
+            kitX = "#starterKit_1";
+            navX = "#navigation_1";
+            
+            kitY = "#starterKit_2";
+            navY = "#navigation_2";    
+        }
+        else{
+            kitX = "#starterKit_2";
+            navX = "#navigation_2";
+            
+            kitY = "#starterKit_1";
+            navY = "#navigation_1";
+        }
+        
+        $(kitX).fadeOut(function(){
+            var items = $(navX).find(".active");
+            
+            $.each(items, function(){
+                $(this).removeClass("active");
+            });
+            
+            
+            $(kitY).fadeIn(function(){
+                $(navY).pushpin({ top: $(navY).offset().top }); 
+            });
+            
+            $('html,body').animate({scrollTop: $("#coming-soon-head-title").offset().top},'fast');
+            
+            return false;
+        });
+    
+    });
+    
+    var options = [
+//        {selector: '#howIdeaLabWorks', offset: 0 ,callback: 'Materialize.fadeInImage("#howIdeaLabWorks")'},
+        {selector: '#ideationProgram', offset: 0, callback: 'Materialize.fadeInImage("#ideationProgram")'},
+        {selector: '#servantProgram', offset: 0, callback: 'Materialize.fadeInImage("#servantProgram")'},
+        {selector: '#volunteerProgram', offset: 0, callback: 'Materialize.fadeInImage("#volunteerProgram")'},
+        {selector: '#facilitationProgram', offset: 0, callback: 'Materialize.fadeInImage("#facilitationProgram")'},
+        {selector: '#capstoneProject', offset: 0, callback: 'Materialize.fadeInImage("#capstoneProject")'},
+        {selector: '#whatIdeaLabExpects', offset: 0 ,callback: 'Materialize.fadeInImage("#whatIdeaLabExpects")'},
+        {selector: '#fellowshipStarterKit', offset: 0 ,callback: 'Materialize.fadeInImage("#fellowshipStarterKit")'},
+        {selector: '#verbTest', offset: 0, callback: 'Materialize.fadeInImage("#verbTest")'},
+        {selector: '#leadershipStory', offset: 0, callback: 'Materialize.fadeInImage("#leadershipStory")'},
+        {selector: '#whyHowWhat', offset: 0 ,callback: 'Materialize.fadeInImage("#whyHowWhat")'},
+        {selector: '#strengthsTest', offset: 0 ,callback: 'Materialize.fadeInImage("#strengthsTest")'}
+    ];
+    
+    Materialize.scrollFire(options);
+});
